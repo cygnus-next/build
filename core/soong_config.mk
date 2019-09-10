92213ac116dff266d3c743a7c9353329c713aebc
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -327,6 +327,8 @@ $(call add_json_str,  ProductManufacturer, $(PRODUCT_MANUFACTURER))
 $(call add_json_str,  ProductBrand,        $(PRODUCT_BRAND))
 $(call add_json_list, BuildVersionTags,    $(BUILD_VERSION_TAGS))
 
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
+
 $(call json_end)
 
 $(file >$(SOONG_VARIABLES).tmp,$(json_contents))
