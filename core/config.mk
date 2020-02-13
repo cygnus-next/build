1ecc94a894c3369107de0675497aedd785cdbad7
Dyneteve <dyneteve@pixelexperience.org>
2020-02-13 18:45:39 +0000

    build: Include BoardConfigCygnus
    Change-Id: I5409bc1a4e0b5e09693364ab7d107d9e620f9d50

--- core/config.mk
+++ core/config.mk
@@ -410,6 +410,8 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 # be device and hardware independent.
 $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
+include vendor/cygnus/config/BoardConfigCygnus.mk
+
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
